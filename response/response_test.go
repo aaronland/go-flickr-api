b3634d6d3fb4d5966c203d25e71fb37a7c1b5d78
thisisaaronland <aaron@localhost>
2021-04-13 12:45:05 -0700

    working on general response tests

--- response/response_test.go
+++ response/response_test.go
@@ -1,5 +1,42 @@
 package response
 
 import (
-	_ "testing"
+	"strings"
+	"testing"
 )
+
+func TestUnmarshalResponse(t *testing.T) {
+
+	rsp := `<?xml version="1.0" encoding="utf-8" ?>
+<rsp stat="ok"></rsp>`
+
+	fh := strings.NewReader(rsp)
+
+	r, err := UnmarshalResponse(fh)
+
+	if err != nil {
+		t.Fatalf("Failed to unmarshal response, %v", err)
+	}
+
+	if r.Status != "ok" {
+		t.Fatalf("Unexpected status, %s", r.Status)
+	}
+}
+
+func TestUnmarshalErrorResponse(t *testing.T) {
+
+	rsp := `<?xml version="1.0" encoding="utf-8" ?>
+<rsp stat="error"><err code="999" msg="This is an error" /></rsp>`
+
+	fh := strings.NewReader(rsp)
+
+	r, err := UnmarshalResponse(fh)
+
+	if err != nil {
+		t.Fatalf("Failed to unmarshal response, %v", err)
+	}
+
+	if r.Error == nil {
+		t.Fatal("Failed to parse error")
+	}
+}
